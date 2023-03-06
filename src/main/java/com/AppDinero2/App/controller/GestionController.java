8bdfe90edab8716e0bffbc562037ab033245da58
franco ruiz <ruizfranco54@gmail.com>
2023-03-06 19:06:36 -0300

    agregado de boton en GASTO

--- src/main/java/com/AppDinero2/App/controller/GestionController.java
+++ src/main/java/com/AppDinero2/App/controller/GestionController.java
@@ -25,20 +25,14 @@ public class GestionController {
 
     @Autowired
     private IngresosRepository ingresosRepository;
+    private Integer id;
+    private int dineroCambiar;
 
     @GetMapping
     public String bienvenido(Model model) {
         Dinero dinero = dineroRepository.getReferenceById(1L);
         model.addAttribute("saldo", "$" + dinero.getSaldo());
         return "index";
-    }
-
-
-    @GetMapping("/verGastos")
-    public String verGastos(Model model) {
-        List<Gasto> gastos = gastoRepository.findAll();
-        model.addAttribute("gastos", gastos);
-        return "verGastos";
     }
 
 
@@ -53,7 +47,7 @@ public class GestionController {
     public String ingresarDinero(@ModelAttribute("atIngresar") Ingreso ingreso) {
         ingresosRepository.save(ingreso);
         Dinero dinero1 = dineroRepository.getReferenceById(1L);
-        dinero1.setSaldo((int) (dinero1.getSaldo()+ingreso.getIngreso()));
+        dinero1.setSaldo((int) (dinero1.getSaldo() + ingreso.getIngreso()));
         dineroRepository.save(dinero1);
         return "redirect:/verIngresos";
     }
@@ -84,5 +78,42 @@ public class GestionController {
 
     }
 
+    @GetMapping("/verGastos")
+    public String verGastos(Model model) {
+        List<Gasto> gastos = gastoRepository.findAll();
+        model.addAttribute("gastos", gastos);
+        return "verGastos";
+    }
+
+
+    //botones
+    @GetMapping("/editarGasto/{id}/{dineroCambiar}")
+    public String editarGasto(Model model, @PathVariable Integer id, @PathVariable int dineroCambiar) {
+        this.id = id;
+        this.dineroCambiar = dineroCambiar;
+        model.addAttribute("edGasto", new Gasto());
+        return "editarGasto";
+    }
+
+    @PostMapping("/editar")
+    public String edGasto(@ModelAttribute("edGasto") Gasto gasto) {
+        gasto.setId(this.id);
+        Dinero dinero = dineroRepository.getReferenceById(1L);
+        dinero.setSaldo(dinero.getSaldo() - dineroCambiar + gasto.getGasto());
+        dineroRepository.save(dinero);
+        gastoRepository.save(gasto);
+        return "redirect:/verGastos";
+
+    }
+
+    @GetMapping("/eliminarGasto/{id}/{dineroCambiar}")
+    public String eliminarGasto(@PathVariable Integer id, @PathVariable int dineroCambiar) {
+        this.dineroCambiar= dineroCambiar;
+        Dinero dinero=dineroRepository.getReferenceById(1L);
+        dinero.setSaldo(dinero.getSaldo()+dineroCambiar);
+        dineroRepository.save(dinero);
+        gastoRepository.deleteById(id);
+        return "redirect:/verGastos";
+    }
 
 }
